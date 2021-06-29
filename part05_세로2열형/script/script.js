ef81e60add1cabfde0828a3e5b936f1eb26d77a1
sun980618 <koarera98@gmail.com>
2021-06-29 15:37:13 +0900

    0629_GNB메뉴구현_세로2열형

---
+++ part05_세로2열형/script/script.js
@@ -0,0 +1,11 @@
+/*eslint-disable*/
+
+$(function(){
+   $("li.mainLi").mouseover(function(){
+      $(this).children("ul.subMenu").stop().fadeIn(300);
+   });
+
+    $("li.mainLi").mouseout(function(){
+      $(this).children("ul.subMenu").stop().hide();
+   });
+});
